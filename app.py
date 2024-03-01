5c50d90b7dff3aff01c1ac4b1a3985bb1d2b67a4
jaryddml <109385281+jaryddml@users.noreply.github.com>
2024-03-01 13:41:32 -0500

    Update app.py
    Change to port 8080

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def results():
     return render_template('results.html', results=aggregated_results)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
